21c0ec7ca6df4c57485db3eed10d4f2a09277f3f
ajdenney12 <ajdenney12@gmail.com>
2026-04-05 15:17:18 -0400

    Increase keyboard vertical offset for iOS

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -119,7 +119,7 @@ export default function ChatScreen() {
         <KeyboardAvoidingView
           behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
           style={styles.chatContainer}
-          keyboardVerticalOffset={Platform.OS === 'ios' ? 100 : 0}
+          keyboardVerticalOffset={Platform.OS === 'ios' ? 120 : 0}
         >
           <ScrollView
             ref={scrollViewRef}
